9bf9048b059c61e9b574f2d6004974f7bb93d00c
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-06-07 15:24:19 +0200

    migrations chat

---
+++ backend/app/Http/Controllers/LobbyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lobby;
+use App\Http\Requests\StoreLobbyRequest;
+use App\Http\Requests\UpdateLobbyRequest;
+
+class LobbyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLobbyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lobby $lobby)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lobby $lobby)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLobbyRequest $request, Lobby $lobby)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lobby $lobby)
+    {
+        //
+    }
+}
