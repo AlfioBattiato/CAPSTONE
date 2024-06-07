9267c1a97fc10baad8315500a17d190f70494fe2
ALFIO BATTIATO <battiato3@gmail.com>
2024-06-07 10:15:10 +0200

    vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
     <BrowserRouter>
 
@@ -11,4 +15,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
