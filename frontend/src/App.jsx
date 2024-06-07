6bad35896d72a8385c90ab7077abccde70bfac38
ALFIO BATTIATO <battiato3@gmail.com>
2024-06-07 10:30:39 +0200

    pronti

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +7,7 @@ function App() {
 
   return (
     <BrowserRouter>
-
+    
 
     </BrowserRouter>
   )
