91898120b7cb48247b62dcdee7ccdf17418d28a1
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-06-10 16:13:44 +0200

    routes password

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,16 +2,32 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ChatController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 
 // Authentication routes provided by Breeze
-Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+Route::group(['middleware' => ['guest']], function () {
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])->name('password.store');
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)->name('verification.verify');
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])->name('verification.send');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
 // Routes protected by authentication
 Route::middleware('auth:sanctum')->group(function () {
@@ -41,9 +57,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('messages', [MessageController::class, 'store']);
     Route::put('messages/{message}', [MessageController::class, 'update']);
     Route::delete('messages/{message}', [MessageController::class, 'destroy']);
-});
-
-// Route to get the authenticated user
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
--- frontend/src/components/MyNavbar.jsx
+++ frontend/src/components/MyNavbar.jsx
@@ -1,16 +1,15 @@
 // import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 // import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link, useNavigate,  } from 'react-router-dom';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link, useNavigate } from "react-router-dom";
 import { BiSolidLogIn } from "react-icons/bi";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
-import { LOGOUT } from '../redux/actions';
+import { LOGOUT } from "../redux/actions";
 // import { CgMenuGridR } from "react-icons/cg";
-
 
 function MyNavbar() {
   // const location = useLocation();
@@ -28,38 +27,39 @@ function MyNavbar() {
       {[false].map((expand) => (
         <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
           <Container fluid>
-          <Link to={'/'}className='navbar-brand'>TrailBlazers </Link>
+            <Link to={"/"} className="navbar-brand">
+              TrailBlazers{" "}
+            </Link>
 
-            {!user ? <Link to={'/login'} > <BiSolidLogIn />Login</Link>
-              : (
-                <>
-                  <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}
-                    className='border-0'>
-
-                  </Navbar.Toggle>
-                  <Navbar.Offcanvas
-                    id={`offcanvasNavbar-expand-${expand}`}
-                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                    placement="end"
-                  >
-                    <Offcanvas.Header closeButton>
-                      <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                        TrailBlazers
-                      </Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                      <Nav className="justify-content-end flex-grow-1 pe-3">
-
-                        <>
-                          <Link  to={`/dashboard/${user.id}`}>Profile</Link>
-                          <Link to={'/'}>Welcome</Link>
-                          <Link to={'/Homepage'}>Home</Link>
-                          <Link onClick={logout} className="dropdown-item border-0 ps-1">
+            {!user ? (
+              <Link to={"/login"}>
+                {" "}
+                <BiSolidLogIn />
+                Login
+              </Link>
+            ) : (
+              <>
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} className="border-0"></Navbar.Toggle>
+                <Navbar.Offcanvas
+                  id={`offcanvasNavbar-expand-${expand}`}
+                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                  placement="end"
+                >
+                  <Offcanvas.Header closeButton>
+                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>TrailBlazers</Offcanvas.Title>
+                  </Offcanvas.Header>
+                  <Offcanvas.Body>
+                    <Nav className="justify-content-end flex-grow-1 pe-3">
+                      <>
+                        <Link to={`/dashboard/${user.id}`}>Profile</Link>
+                        <Link to={"/"}>Welcome</Link>
+                        <Link to={"/Homepage"}>Home</Link>
+                        <Link onClick={logout} className="dropdown-item border-0 ps-1">
                           Logout
                         </Link>
-                          {/* <Nav.Link href="#action2">Link</Nav.Link> */}
+                        {/* <Nav.Link href="#action2">Link</Nav.Link> */}
 
-                          {/* <Form className="d-flex">
+                        {/* <Form className="d-flex">
                         <Form.Control
                           type="search"
                           placeholder="Search"
@@ -68,20 +68,15 @@ function MyNavbar() {
                         />
                         <Button variant="outline-success">Search</Button>
                       </Form> */}
-                        </>
-
-
-                      </Nav>
-
-                    </Offcanvas.Body>
-                  </Navbar.Offcanvas></>
-              )
-            }
-
-          </Container >
-        </Navbar >
-      ))
-      }
+                      </>
+                    </Nav>
+                  </Offcanvas.Body>
+                </Navbar.Offcanvas>
+              </>
+            )}
+          </Container>
+        </Navbar>
+      ))}
     </>
   );
 }
