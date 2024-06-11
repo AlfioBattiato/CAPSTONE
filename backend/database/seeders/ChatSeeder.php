f064e9097e00bac9a7155e6f810b30bfb4b9c600
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-06-11 16:24:40 +0200

    we can see chats

--- backend/database/seeders/ChatSeeder.php
+++ backend/database/seeders/ChatSeeder.php
@@ -20,13 +20,13 @@ class ChatSeeder extends Seeder
 
         if ($user1 && $user2 && $user3) {
 
-            $chat1 = Chat::create(['name' => 'Chat between admin and guest']);
+            $chat1 = Chat::create(['name' => null]);
             $chat1->users()->attach([$user1->id, $user2->id]);
 
-            $chat2 = Chat::create(['name' => 'Chat between admin and user3']);
+            $chat2 = Chat::create(['name' => null]);
             $chat2->users()->attach([$user1->id, $user3->id]);
 
-            $chat3 = Chat::create(['name' => 'Chat between guest and user3']);
+            $chat3 = Chat::create(['name' => null]);
             $chat3->users()->attach([$user2->id, $user3->id]);
 
 
